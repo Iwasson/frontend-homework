40a59382509360314c969f9c040054e6d2823795
Iwasson <Iwasson@users.noreply.github.com>
2023-04-26 10:14:09 -0700

    Adding Form exercise

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function formSubmit(event) {
+  let fullName = event.target[0].value;
+  let email = event.target[1].value;
+  let registration = event.target[2].value;
+  let courseOne = event.target[3].value;
+  let courseTwo = event.target[4].value;
+  let courseThree = event.target[5].value;
+  let extra = event.target[6].value;
+
+  let courses = "";
+  if(event.target[3].checked)
+    courses += ` ${courseOne}`;
+  if(event.target[4].checked)
+    courses += ` ${courseTwo}`;
+  if(event.target[5].checked)
+    courses += ` ${courseThree}`;
+  courses = courses.trim();
+  console.log(`Full Name: ${fullName}\nEmail: ${email}\nRegistration: ${registration}\nCourses: ${courses}\nAnything Else: ${extra}`);
+  event.preventDefault();
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", formSubmit);
