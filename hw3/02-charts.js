2c7d9befd72be1edadd55be386ef96f8b0f50efd
Iwasson <59637502+Iwasson@users.noreply.github.com>
2023-05-09 21:57:00 -0700

    Submitting part 2 of hw3

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -28,25 +28,72 @@ const borderColors = [
 
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+document.onload = getApiData(url);
 
-const renderChart = () => {
-  const donutChart = document.querySelector('.donut-chart');
+function getApiData(url) {
+  let houses = [];
 
-  new Chart(donutChart, {
-    type: 'doughnut',
-    data: {
-      labels: ['label', 'label', 'label', 'label'],
-      datasets: [
-        {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
-          backgroundColor: backgroundColors,
-          borderColor: borderColors,
-          borderWidth: 1,
-        },
-      ],
-    },
+  fetch(url).then(response => response.json().then(data => {
+    data.forEach(character => {
+      let family = character.family
+      // check if the family starts with the word house, strip it if it does
+      if(family.startsWith("House"))
+        family = family.substring("House".length+1);
+      
+      // check if the family is none or some variant of unknown
+      if(family == ""         || 
+         family == "None"     ||
+         family == "Unkown"   ||
+         family == "Unknown"    ) {
+        if(!houses["None"])
+          houses["None"] = 1;
+        else
+          houses["None"] += 1;
+      }
+      else {
+        // check all of the house names, if there is a single letter difference
+        // between the family we are adding and a family thats already present,
+        // then assume that there was a typo and reassign family to be that key
+        Object.keys(houses).forEach(house => {
+          if(stringDiff(house, family) < 2) {
+            family = house;
+          }
+        });
+        
+        if(!houses[family])
+          houses[family] = 1;
+        else
+          houses[family] += 1;
+      }
+    });
+    
+    const donutChart = document.querySelector('.donut-chart');
+
+    new Chart(donutChart, {
+      type: 'doughnut',
+      data: {
+        labels: Object.keys(houses),
+        datasets: [
+          {
+            label: 'Characters Per House',
+            data: Object.values(houses),
+            backgroundColor: backgroundColors,
+            borderColor: borderColors,
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+  }));
+}
+
+function stringDiff(stringOne, stringTwo) {
+  // returns the number of letters that dont match between string one and string two
+  let count = 0;
+  stringTwo.split("").forEach((letter, pos) => {
+    if(letter != stringOne[pos])
+      count += 1;
   });
-};
 
-renderChart();
+  return count;
+}
