5d19e8817c29acbef286c9992746ba267c1c8e5c
Iwasson <59637502+Iwasson@users.noreply.github.com>
2023-05-09 21:26:17 -0700

    Finishing part 1 of hw3

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,38 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+const section = document.querySelector("section");
 
-fetch(url);
+document.onload = getApiData(url);
+
+function getApiData(url) {
+  fetch(url).then(response => response.json().then(data => {
+    data.forEach(character => {
+      section.appendChild(makeCharacterCard(character.fullName, character.title, character.imageUrl));
+    });
+  }));
+}
+
+function makeCharacterCard(characterName, characterTitle, imageUrl) {
+  let newCard = document.createElement("div");
+  let image = document.createElement("img");
+  let name = document.createElement("p");
+  let title = document.createElement("p");
+
+  image.src = imageUrl;
+  name.textContent = characterName;
+  title.textContent = characterTitle;
+
+  newCard.classList.add("character");
+  newCard.classList.add("text-center");
+  newCard.classList.add("ms-2");
+  name.classList.add("fw-bolder");
+  name.classList.add("fs-4");
+  title.classList.add("fw-bold");
+  image.classList.add("charImg");
+
+  newCard.appendChild(image);
+  newCard.appendChild(name);
+  newCard.appendChild(title);
+
+  return newCard;
+}
